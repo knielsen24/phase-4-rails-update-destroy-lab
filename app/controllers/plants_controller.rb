60db163fe32bc1d5c0557614c09dc1cea6b9b82c
Kevin Nielsen <knielsen24@gmail.com>
2022-06-28 16:02:20 -0700

    commit tree b0eb6fa2df092ac780672adefffa9ea2fa6dca91

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,26 +1,46 @@
 class PlantsController < ApplicationController
 
   # GET /plants
-  def index
-    plants = Plant.all
-    render json: plants
-  end
+	def index
+		plants = Plant.all
+		render json: plants
+	end
 
-  # GET /plants/:id
-  def show
-    plant = Plant.find_by(id: params[:id])
-    render json: plant
-  end
+	# GET /plants/:id
+	def show
+		plant = Plant.find_by(id: params[:id])
+		render json: plant
+	end
 
-  # POST /plants
-  def create
-    plant = Plant.create(plant_params)
-    render json: plant, status: :created
-  end
+	# POST /plants
+	def create
+		plant = Plant.create(plant_params)
+		render json: plant, status: :created
+	end
 
-  private
+	def update
+		plant = Plant.find_by(id: params[:id])
+		if plant
+			plant.update(plant_params)
+			render json: plant, status: :accepted
+		else
+			render json: { error: 'Plant not found' }, status: :not_found
+		end
+	end
 
-  def plant_params
-    params.permit(:name, :image, :price, :is_in_stock)
-  end
+	def destroy
+		plant = Plant.find_by(id: params[:id])
+		if plant
+			plant.destroy
+			head :no_content
+		else
+			render json: { error: 'Plant not found' }, status: :not_found
+		end
+	end
+
+	private
+
+	def plant_params
+		params.permit(:name, :image, :price, :is_in_stock)
+	end
 end
